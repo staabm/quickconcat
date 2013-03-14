72c37544f365555e5b33759d2e5093647fe59616
Markus Staab <maggus.staab@googlemail.com>
2013-03-14 11:27:08 +0100

    added support for multiple asset roots. added bad-request http-400 status indicators. tabs to spaces.

--- quickconcat.php
+++ quickconcat.php
@@ -1,16 +1,15 @@
 <?php
 /*
 quickconcat: a simple dynamic concatenator for html, css, and js files
-	Copyright 2012, Scott Jehl, Filament Group, Inc. Dual licensed under MIT and GPLv2
-	*  accepts 2 query strings:
-		* files (required): a comma-separated list of root-relative file paths
-		* wrap (optional): Enclose each result in an element node with url attribute? False by default.
+    Copyright 2012, Scott Jehl, Filament Group, Inc. Dual licensed under MIT and GPLv2
+    *  accepts 2 query strings:
+        * files (required): a comma-separated list of root-relative file paths
+        * wrap (optional): Enclose each result in an element node with url attribute? False by default.
 */
 
-function is_file_in_scope( $file ){
-	$appRoot = dirname(__FILE__);
-	$realpath = realpath($file);
-	return ($file && is_file($realpath) && strpos($realpath, $appRoot) === 0);
+function is_file_in_scope( $file, $assetRoot){
+    $realpath = realpath($file);
+    return ($file && is_file($realpath) && strpos($realpath, $assetRoot) === 0) ? $realpath : false;
 }
 
 // List of files, comma-separated paths
@@ -20,46 +19,61 @@ $filelist = $_REQUEST[ "files" ];
 $relativeroot = "";
 
 // get the public path to this file, plus the baseurl
-$pubroot = dirname( $_SERVER['PHP_SELF'] ) . "/" . $relativeroot;
+$pubroot = dirname( $_SERVER['PHP_SELF'] ) . $relativeroot;
 
 // Enclose each result in an element node with url attribute?
 $wrap = isset( $_REQUEST[ "wrap" ] );
 
 // Get the filetype and array of files
 if ( ! isset( $filelist ) ){
-	echo '$files must be specified!';
-	exit;
+    header('HTTP/1.1 400 Bad Request');
+    echo '$files must be specified!';
+    exit;
 }
 
 $files = explode( ",", $filelist );
+$assetRoots = array('.');
 
 // sanitize file-parameters
 foreach ( $files as $idx => $file ) {
-	// we allow only certain filetypes
-	if( !( preg_match( '/\.(js|html|css)$/', $file ) && is_file_in_scope($file) ) ){
-		unset($files[$idx]);
-	}
+    $found = false;
+    foreach($assetRoots as $assetRoot) {
+        $realpath = $assetRoot . ltrim($file, '.~/\\');
+    
+        // santize url params: make sure the file which will be served is contained within an appRoot
+        if ($actualPath = is_file_in_scope($realpath, $assetRoot)) {
+            $files[$idx] = $actualPath;
+            $found = true;
+            break;
+        }
+    }
+    // we allow only certain filetypes
+    if( !preg_match( '/\.(js|html|css)$/', $file ) || !$found ){
+        unset($files[$idx]);
+    }
 }
 
 $files = array_values( $files );
 if( count( $files ) == 0 ){
-	exit;
+    header('HTTP/1.1 400 Bad Request');
+    echo 'no valid files specified!';
+    exit;
 }
 
 $lmodified = 0;
 
 // build last-modified date for the file-bundle
 foreach ( $files as $file ) {
-	$mtime = filemtime($file);
-	if($mtime > $lmodified) {
-		$lmodified = $mtime;
-	}
+    $mtime = filemtime($file);
+    if($mtime > $lmodified) {
+        $lmodified = $mtime;
+    }
 }
 
 // fast exit, in case the browsers-cache is up2date
 if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && $lmodified > 0 && $_SERVER['HTTP_IF_MODIFIED_SINCE'] == $lmodified) {
-	header('HTTP/1.1 304 Not Modified');
-	exit();
+    header('HTTP/1.1 304 Not Modified');
+    exit();
 }
 // Guess file type
 $fext = preg_match( '/\.(js|html|css)$/', $files[ 0 ], $match );
@@ -69,36 +83,36 @@ $type = "text/" . ( $ftype === "js" ? "javascript" : $ftype );
 // Set the content type and last-modified headers
 header('Content-Type: ' . $type);
 if ($lmodified > 0) {
-	header('Last-Modified: '. $lmodified);
+    header('Last-Modified: '. $lmodified);
 }
 
 // Loop through the files
 foreach ( $files as $file ) {
-	echo $wrap ? "<entry url=\"". $file . "\">" : "";
-	
-	//prefix relative CSS paths
-	// TODO: HTML as well
-	if( $ftype === "css" ){
-		$contents = file_get_contents($relativeroot . $file);
-		
-		$prefix = $pubroot . dirname($file) . "/";
-		$contents = preg_replace( '/(url\(["\']?)([^\/"\'])([^\:\)]+["\']?\))/i', "$1" . $prefix .  "$2$3", $contents );
-		
-		echo $contents;
-		unset ($contents);
-	} else {
-		// all remaining types can be streamed as-is to the browser
-		readfile($relativeroot . $file);		
-	}
-	
-	if( $ftype === "js" ){
-		// add an extra semicolon, so ASI and non-ASI js-files will not clash
-		echo ';';
-	}
-	
-	echo $wrap ? "</entry>\n" : "";
-	
-	// flush-ing the content will change the transfer-encoding to "chunked"
-	// and makes http-body as early as possible available to the client 
-	flush();
+    echo $wrap ? "<entry url=\"". $file . "\">" : "";
+    
+    //prefix relative CSS paths
+    // TODO: HTML as well
+    if( $ftype === "css" ){
+        $contents = file_get_contents($file);
+        
+        $prefix = $pubroot . ltrim(dirname($file), "/") . "/";
+        $contents = preg_replace( '/(url\(["\']?)([^\/"\'])([^\:\)]+["\']?\))/i', "$1" . $prefix .  "$2$3", $contents );
+            
+        echo $contents;
+        unset ($contents);
+    } else {
+        // all remaining types can be streamed as-is to the browser
+        readfile($file);        
+    }
+    
+    if( $ftype === "js" ){
+        // add an extra semicolon, so ASI and non-ASI js-files will not clash
+        echo ';';
+    }
+    
+    echo $wrap ? "</entry>\n" : "";
+    
+    // flush-ing the content will change the transfer-encoding to "chunked"
+    // and makes http-body as early as possible available to the client 
+    flush();
 }
